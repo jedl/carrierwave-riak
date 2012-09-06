f7ea3c70f0e846e18220de542461157c219fbca1
jedl <jed.luhmann@whcc.com>
2012-09-06 14:10:55 -0500

    Commented out build_version_key code.

--- lib/carrierwave/uploader/riak.rb
+++ lib/carrierwave/uploader/riak.rb
@@ -29,32 +29,32 @@ module CarrierWave
       #   end
       # 
       # end
-
-      def store_versions!(new_file)
-        active_versions.each { |name, v|
-          # v.key = build_versioned_key(key, name)
-          v.store!(new_file)
-        }
-      end
-
-      def remove_versions!
-        versions.each { |name, v|
-          # v.key = build_versioned_key(key, name)
-          v.remove!
-        }
-      end
-
-      def retrieve_versions_from_cache!(cache_name)
-        versions.each { |name, v|
-          # v.key = build_versioned_key(key, name)
-          v.retrieve_from_cache!(cache_name)
-        }
-      end
-
-      def retrieve_versions_from_store!(identifier)
-        versions.each { |name, v|
-          v.retrieve_from_store!(build_versioned_key(identifier, name)) }
-      end
+      # 
+      # def store_versions!(new_file)
+      #   active_versions.each { |name, v|
+      #     # v.key = build_versioned_key(key, name)
+      #     v.store!(new_file)
+      #   }
+      # end
+      # 
+      # def remove_versions!
+      #   versions.each { |name, v|
+      #     # v.key = build_versioned_key(key, name)
+      #     v.remove!
+      #   }
+      # end
+      # 
+      # def retrieve_versions_from_cache!(cache_name)
+      #   versions.each { |name, v|
+      #     # v.key = build_versioned_key(key, name)
+      #     v.retrieve_from_cache!(cache_name)
+      #   }
+      # end
+      # 
+      # def retrieve_versions_from_store!(identifier)
+      #   versions.each { |name, v|
+      #     v.retrieve_from_store!(build_versioned_key(identifier, name)) }
+      # end
     end
   end
 end
